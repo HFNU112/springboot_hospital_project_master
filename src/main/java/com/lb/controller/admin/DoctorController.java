dbdc3aaf3914cd7ad568cf30d73b166648dcdc79
hfnu_2010211112 <2942885033@qq.com>
2023-06-05 22:15:17 +0800

    chore&docs update file

--- src/main/java/com/lb/controller/admin/DoctorController.java
+++ src/main/java/com/lb/controller/admin/DoctorController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -21,7 +22,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/admin/doctor")
 public class DoctorController {
-    @Autowired
+
+    @Resource
     private LbDoctorService lbDoctorService;
 
     @RequestMapping("/manage")
@@ -31,12 +33,12 @@ public class DoctorController {
                                @RequestParam(required = false) String certId,
                                Model model) {
         //查询医生的集合数据
-        PageQuery<LbDoctor> page = lbDoctorService.findList(pageNo,pageSize,name,certId);
-        model.addAttribute("page",page);
-        model.addAttribute("pageNo",pageNo);
-        model.addAttribute("name",name);
-        model.addAttribute("certId",certId);
-        model.addAttribute("path","/admin/doctor/manage");
+        PageQuery<LbDoctor> page = lbDoctorService.findList(pageNo, pageSize, name, certId);
+        model.addAttribute("page", page);
+        model.addAttribute("pageNo", pageNo);
+        model.addAttribute("name", name);
+        model.addAttribute("certId", certId);
+        model.addAttribute("path", "/admin/doctor/manage");
         return "admin/doctorManage";
     }
 
@@ -44,19 +46,21 @@ public class DoctorController {
      * 医生新增
      */
     @RequestMapping("/")
-    public String doctorAddForm(LbDoctor lbDoctor,Model model) {
-        model.addAttribute("doctor",lbDoctor);
+    public String doctorAddForm(LbDoctor lbDoctor, Model model) {
+        model.addAttribute("doctor", lbDoctor);
         return "admin/doctorForm";
     }
 
     /**
      * 医生编辑
+     *
      * @param model
      * @return
      */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String doctorEditForm(@PathVariable Integer id,Model model) {
-        model.addAttribute("doctor",lbDoctorService.findOne(id));
+    @ResponseBody
+    public String doctorEditForm(@PathVariable Integer id, Model model) {
+        model.addAttribute("doctor", lbDoctorService.findOne(id));
         return "admin/doctorForm";
     }
 
@@ -71,6 +75,7 @@ public class DoctorController {
 
     /**
      * 异步更新记录
+     *
      * @param lbDoctor
      * @return
      */
@@ -84,8 +89,8 @@ public class DoctorController {
      * 异步删除
      */
     @ResponseBody
-    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
-    public ResponseResult delete(@PathVariable Integer id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseResult delete(@PathVariable Integer id) {
         return lbDoctorService.deleteDoctor(id);
     }
 
@@ -94,7 +99,7 @@ public class DoctorController {
      */
     @ResponseBody
     @RequestMapping(value = "/getList/{department}")
-    public List<LbDoctor> getList(@PathVariable String department){
+    public List<LbDoctor> getList(@PathVariable String department) {
         return lbDoctorService.getListByDepartment(department);
     }
 }
